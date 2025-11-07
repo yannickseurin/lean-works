5c079c9a676bccff834fb261241303d167835863
Yannick Seurin <yannick.seurin@gmail.com>
2025-11-07 09:56:03 +0100

    omit draft files from build

--- LeanWorks.lean
+++ LeanWorks.lean
@@ -1,5 +1,5 @@
 -- This module serves as the root of the `LeanWorks` library.
 -- Import modules here that should be built as part of the library.
 import LeanWorks.Negligible
-import LeanWorks.Ddh
-import LeanWorks.Probability
+-- import LeanWorks.Ddh
+-- import LeanWorks.Probability
