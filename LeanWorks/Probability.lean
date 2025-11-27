e91b182d4ebca72e58d5580cad52cda762ff6f7c
Yannick Seurin <yannick.seurin@gmail.com>
2025-11-27 12:48:25 +0100

    universes; simplify proof of map_eq_uniform_of_bijective

--- LeanWorks/Probability.lean
+++ LeanWorks/Probability.lean
@@ -10,12 +10,14 @@ namespace PMF
 
 section PMFBind
 
-variable {α β : Type}
+universe u
+
+variable {α β : Type u}
          (p : PMF α) (f : α → PMF β)
 
 -- variant of `PMF.pure_bind`
 @[simp]
-theorem pure_bind' (a : α) : ((PMF.pure a) >>= f) = f a := pure_bind a f
+theorem pure_bind' (a : α) : ((pure a) >>= f) = f a := pure_bind a f
 
 -- variant of `PMF.bind_apply`
 @[simp]
@@ -48,18 +50,12 @@ lemma bind_skip_const' (pa : PMF α) (pb : PMF β) (f : α → PMF β) :
 
 end PMFBind
 
-noncomputable section Uniform
+noncomputable section UniformProd
 
-variable {α β : Type} [Fintype α] [Nonempty α] [DecidableEq α]
-                      [Fintype β] [Nonempty β] [DecidableEq β]
+universe u
 
-def uniform_then_prod : PMF (α × β) := do
-  let a ← PMF.uniformOfFintype α
-  let b ← PMF.uniformOfFintype β
-  PMF.pure (a, b)
-
-def uniform_then_prod' : PMF (α × β) :=
-    (PMF.uniformOfFintype α) >>= fun x ↦ (PMF.uniformOfFintype β) >>= fun y ↦ PMF.pure (x, y)
+variable {α β : Type u} [Fintype α] [Nonempty α] [DecidableEq α]
+                        [Fintype β] [Nonempty β] [DecidableEq β]
 
 /--
 Drawing `a` from `α` and `b` from `β` and forming the pair `(a, b)`
@@ -72,7 +68,7 @@ lemma uniform_prod :
     do
       let a ← PMF.uniformOfFintype α
       let b ← PMF.uniformOfFintype β
-      PMF.pure (a, b)
+      pure (a, b)
     ) = PMF.uniformOfFintype (α × β) := by
   ext p
   let (a, b) := p
@@ -82,6 +78,16 @@ lemma uniform_prod :
   rw [ENNReal.tsum_mul_left, ← ENNReal.tsum_prod]
   rw [ENNReal.mul_inv_natCast]
   simp [eq_comm]
+
+end UniformProd
+
+noncomputable section UniformBij
+
+universe u v
+
+variable {α : Type u} [Fintype α] [Nonempty α] [DecidableEq α]
+         {β : Type v} [Fintype β] [Nonempty β] [DecidableEq β]
+
 
 /--
 If `f : α → β` is bijective, then drawing `a` uniformly from `α`
@@ -99,52 +105,13 @@ def foo : PMF β := do
 -/
 lemma map_eq_uniform_of_bijective (f : α → β) (hf : Function.Bijective f) :
     PMF.map f (PMF.uniformOfFintype α) = PMF.uniformOfFintype β := by
-  let B := (Fintype.card β : ENNReal)
   ext b
   simp only [map_apply, uniformOfFintype_apply]
   rw [Fintype.card_of_bijective hf]
-  rcases Function.bijective_iff_has_inverse.mp hf with ⟨f', invli, invri⟩
-  let a := f' b
-  let Sa : Finset α := {x | x = a}
-  let Sna : Finset α := {x | x ≠ a}
-  have hu : Finset.univ = Sa ∪ Sna := by
-    ext x
-    simp
-    simp [Sa, Sna]
-    apply Classical.em
-  have hd : Disjoint Sa Sna := by
-    apply Finset.disjoint_left.mpr
-    simp [Sa, Sna]
-  rw [tsum_fintype fun y ↦ if b = f y then B⁻¹ else 0]
-  rw [hu, Finset.sum_union hd]
-  have snazero: ∀ x ∈ Sna, (fun y ↦ if b = f y then B⁻¹ else 0) x = 0 := by
-    intro x xsna
-    dsimp
-    rw [if_neg]
-    apply Finset.mem_filter.mp at xsna
-    intro bfx
-    have : x = a :=
-      calc
-        x = f' (f x) := by exact (invli x).symm
-        _ = f' b := by rw [← bfx]
-        _ = a := rfl
-    tauto
-  have sacardb : ∀ x ∈ Sa, (fun y ↦ if b = f y then B⁻¹ else 0) x = B⁻¹ := by
-    intro x xsa
-    dsimp
-    rw [if_pos]
-    apply Finset.mem_filter.mp at xsa
-    calc
-      b = f (f' b) := by exact (invri b).symm
-      _ = f a := by rfl
-      _ = f x := by rw [xsa.2]
-  rw [Finset.sum_eq_zero snazero, add_zero, Finset.sum_congr rfl sacardb, Finset.sum_const]
-  have cardsa : Finset.card Sa = 1 := by
-    apply (Fintype.existsUnique_iff_card_one _).mp
-    exact existsUnique_eq
-  rw [cardsa]
-  exact one_nsmul B⁻¹
+  rcases Function.bijective_iff_has_inverse'.mp hf with ⟨g, hg⟩
+  simp_rw [hg]
+  simp [tsum_ite_eq]
 
-end Uniform
+end UniformBij
 
 end PMF
